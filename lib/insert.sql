a2d1ad31518d89ae101bc8ca8f64d80dac086acd
github username <chao.alexander.123@gmail.com>
2018-10-14 21:27:13 -0400

    create bear database, insert seed data and defined queries methods

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 10, "Male", "Brown", "Gentle", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "Male", "White", "Aggressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "Female", "Gold", "Gentle", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "Male", "Brown", "Moody", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "Female", "Gold", "Gentle", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "Male", "Black", "Aggressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "Female", "Black", "Gentle", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "Male", "Gold", "Aggressive", true);
